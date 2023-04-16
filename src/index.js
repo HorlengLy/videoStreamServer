8fbb9cc9e2720bda4e4cfca54205ec3f0d2fca14
Horleng <ly.horleng.1221@rupp.edu.kh>
2023-04-16 22:23:52 +0700

    updateCors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:5173","http://127.0.0.1:5500"],
+        origin:["http://localhost:5173","https://videocaller.netlify.app/"],
         methods:["GET","POST"]
         },
 });
